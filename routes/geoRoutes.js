7f2a47ba1498cb246b00018bdca6aa43ab75171a
Daniel Gonzalez <daniel@gmail.com>
2026-07-24 12:55:18 -0500

    post

--- routes/geoRoutes.js
+++ routes/geoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const geoController = require('../controllers/geoController');
 
-router.put('/actualizar', geoController.actualizarUbicacion);
+router.post('/actualizar', geoController.actualizarUbicacion);
 router.get('/monitorear/:amb_id', geoController.obtenerUbicacionActual);
 
 module.exports = router;
